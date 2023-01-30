b2c86db1a4b7d9def97c0ac6b95f105974419ce3
Karam Singh <karamsingh175@gmail.com>
2023-01-29 20:49:26 -0800

    Moving service dependencies into service library

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
-    implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
-    runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.5.1")
+    implementation("com.github.karamvsingh.backend-framework:service-library:0.1.0")
 }
 
 kotlin {
