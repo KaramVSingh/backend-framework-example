e5a13db92fd6f1a00269a4d9bc26405949d7efbb
Karam Singh <karamsingh175@gmail.com>
2023-03-26 01:29:27 -0700

    Adding a hack to support kotlinxserialization

--- service/src/main/kotlin/com/github/karamvsingh/backendframeworkexample/operations/GetResource.kt
+++ service/src/main/kotlin/com/github/karamvsingh/backendframeworkexample/operations/GetResource.kt
@@ -2,10 +2,12 @@ package com.github.karamvsingh.backendframeworkexample.operations
 
 import com.github.karamvsingh.backendframework.servicelibrary.annotations.Operation
 import com.github.karamvsingh.backendframework.servicelibrary.context.Context
+import com.github.karamvsingh.backendframeworkexample.ResourceNotFound
 import com.github.karamvsingh.backendframeworkexample.GetResourceInput
 import com.github.karamvsingh.backendframeworkexample.GetResourceOutput
 
 @Operation("com.github.karamvsingh.backendframeworkexample", "GetResource")
 fun getResource(input: GetResourceInput, context: Context): GetResourceOutput {
+    throw ResourceNotFound("WOW")
     return GetResourceOutput("resource-name")
 }
