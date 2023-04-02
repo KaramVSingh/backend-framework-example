4904103948fd6003ce60c134cf3d23ce7f1d6798
Karam Singh <karamsingh175@gmail.com>
2023-04-02 15:59:44 -0700

    switching to io.github.karamvsingh namespace

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp").version("1.8.0-1.0.8")
     id("com.github.johnrengelman.shadow").version("2.0.4")
-    id("com.github.karamvsingh.backend-framework.gradle-plugin").version("0.1.0")
+    id("io.github.karamvsingh.backend-framework.gradle-plugin").version("0.1.0")
     kotlin("plugin.serialization").version("1.8.0")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.github.karamvsingh.backend-framework:service-library:0.1.0")
+    implementation("io.github.karamvsingh.backend-framework:service-library:0.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
     model(project(":model"))
